16b0ebcebc99ce1ab147c973f19462b6e6c7bff9
sheku-is-a-se <sheku2026@gmail.com>
2026-05-25 23:13:52 -0400

    Add UserInterface main menu and terminal home screen loop

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,45 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public void mainMenu() {
+        String prompt = """
+                ╔══════════════════════════════════════════════╗
+                ║              DELI-CIOUS SANDWICH SHOP        ║
+                ╠══════════════════════════════════════════════╣
+                ║         Create and manage customer orders    ║
+                ╚══════════════════════════════════════════════╝
+                
+                                 HOME SCREEN
+                ------------------------------------------------
+                [1] New Order
+                [0] Exit
+                ------------------------------------------------
+                Enter your choice:\s""";
+
+        boolean running = true;
+
+        do {
+            System.out.println(prompt);
+            int userMenu = PromptParsing.promptForInteger(prompt);
+
+
+            switch (userMenu) {
+                case 1:
+                    System.out.println("New Order!");
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("You have exited the application");
+                    break;
+                default:
+                    System.out.println("Oops! That wasn't a valid option.");
+                    break;
+            }
+        } while (running);
+
+    }
 }
