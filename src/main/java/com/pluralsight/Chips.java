a2fc1bd51fc9982a60e5f9a83f76a1fa8847f9a3
sheku-is-a-se <sheku2026@gmail.com>
2026-05-26 23:24:09 -0400

    Implement getPrice method for Chips class

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
-public class Chips {
+public class Chips extends Product {
+    public Chips(String name, String size, String type) {
+        super(name, size, type);
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50;
+    }
 }
