d28edb9ad52e4d5ce06c2d5b47a08cfaaf20ed42
sheku-is-a-se <sheku2026@gmail.com>
2026-05-26 23:33:43 -0400

    Implement getPrice base calculations for Sandwich class

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -7,15 +7,26 @@ public class Sandwich extends Product {
     private boolean toasted;
     private List<Topping> toppings;
 
-    public Sandwich(String size, String bread, boolean toasted, List<Topping> toppings) {
-        super("Sandwich",size);
+    public Sandwich(String size, String type, String bread, boolean toasted, List<Topping> toppings) {
+        super("Sandwich",size,type);
         this.bread = bread;
         this.toasted = toasted;
         this.toppings = toppings;
     }
 
+
     @Override
     public double getPrice() {
-        return 0;
+        if (this.getSize().equals("4")){
+            return 5.50;
+        } else if (this.getSize().equals("8")) {
+            return 7.50;
+
+        } else if (this.getSize().equals("12")) {
+            return 9.50;
+
+        }
+
+        return 0.0;
     }
 }
