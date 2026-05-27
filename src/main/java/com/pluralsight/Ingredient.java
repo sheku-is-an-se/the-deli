12418bb2a13004840bc01d0a468f92532d9736c8
sheku-is-a-se <sheku2026@gmail.com>
2026-05-27 09:27:55 -0400

    implement fields, constructor, and getters for Ingredient class

--- src/main/java/com/pluralsight/Ingredient.java
+++ src/main/java/com/pluralsight/Ingredient.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
 public abstract class Ingredient {
+
+    private String name;
+    private boolean isExtra;
+    private boolean isPremium;
+    private String type;
+
+    public Ingredient(String name, boolean isExtra, boolean isPremium, String type) {
+        this.name = name;
+        this.isExtra = isExtra;
+        this.isPremium = isPremium;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
