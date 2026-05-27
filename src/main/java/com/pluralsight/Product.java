9f4be84b8a928f2cce4efbf67413a42c0e930796
sheku-is-a-se <sheku2026@gmail.com>
2026-05-26 23:23:15 -0400

    Add size and type getter methods to Product abstract class

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -3,10 +3,20 @@ package com.pluralsight;
 public abstract class Product implements Priceable {
     private String name;
     private String size;
+    private String type;
 
-    public Product(String name, String size) {
+    public Product(String name, String size, String type) {
         this.name = name;
         this.size = size;
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public abstract double getPrice();
