398c187a92f08648f0f765508d89368bdbedbe81
sheku-is-a-se <sheku2026@gmail.com>
2026-05-27 16:17:56 -0400

    Complete display for order screen in UserInterface

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -8,7 +8,7 @@ public class UserInterface {
     public void homeMenu() {
         String prompt = """
                 ╔══════════════════════════════════════════════╗
-                ║              DELI-CIOUS SANDWICH SHOP        ║
+                ║              THE-DELI SANDWICH SHOP        ║
                 ╠══════════════════════════════════════════════╣
                 ║         Create and manage customer orders    ║
                 ╚══════════════════════════════════════════════╝
@@ -28,7 +28,7 @@ public class UserInterface {
 
             switch (userMenu) {
                 case 1:
-                    System.out.println("New Order!");
+                    orderScreen();
                     break;
                 case 0:
                     running = false;
@@ -45,9 +45,9 @@ public class UserInterface {
     public void orderScreen() {
         String prompt = """
                 ╔══════════════════════════════════════════════╗
-                ║              DELI-CIOUS SANDWICH SHOP        ║
+                ║              THE-DELI SANDWICH SHOP          ║
                 ╠══════════════════════════════════════════════╣
-                ║         Create and manage customer orders    ║
+                ║               Build and manage order         ║
                 ╚══════════════════════════════════════════════╝
                 
                                  ORDER SCREEN
@@ -55,7 +55,8 @@ public class UserInterface {
                 [1] Add Sandwich
                 [2] Add Drink
                 [3] Add Chips
-                [4] Add Drink
+                [4] Checkout
+                [0] Cancel Order
                 ------------------------------------------------
                 Enter your choice:\s""";
 
@@ -67,11 +68,65 @@ public class UserInterface {
 
             switch (userMenu) {
                 case 1:
-                    System.out.println("New Order!");
+                    sandwichScreen();
+                    break;
+                case 2:
+                    System.out.println("Add Drink");
+                    break;
+                case 3:
+                    System.out.println("Add Chips!");
+                    break;
+                case 4:
+                    System.out.println("Checkout");
                     break;
                 case 0:
                     running = false;
-                    System.out.println("You have exited the application");
+                    System.out.println("Cancel order");
+                    break;
+                default:
+                    System.out.println("Oops! That wasn't a valid option.");
+                    break;
+            }
+        } while (running);
+
+    }
+
+    public void sandwichScreen() {
+        String prompt = """
+                ╔══════════════════════════════════════════════╗
+                ║                 THE-DELI                     ║
+                ╠══════════════════════════════════════════════╣
+                ║            Customize your sandwich           ║
+                ╚══════════════════════════════════════════════╝
+                
+                                ADD SANDWICH
+                ------------------------------------------------
+                Follow the steps to build your sandwich.
+                ------------------------------------------------
+                """;
+
+        boolean running = true;
+
+        do {
+            int userMenu = PromptParsing.promptForInteger(prompt);
+
+
+            switch (userMenu) {
+                case 1:
+                    System.out.println("Add Sandwich!");
+                    break;
+                case 2:
+                    System.out.println("Add Drink");
+                    break;
+                case 3:
+                    System.out.println("Add Chips!");
+                    break;
+                case 4:
+                    System.out.println("Checkout");
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Cancel order");
                     break;
                 default:
                     System.out.println("Oops! That wasn't a valid option.");
