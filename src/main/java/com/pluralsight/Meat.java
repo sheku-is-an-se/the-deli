adfe75b8a4cfa11d63c6a099f4e40734f29e34f0
sheku-is-a-se <sheku2026@gmail.com>
2026-05-27 20:54:50 -0400

    Refactor Meat class for universal Size

--- src/main/java/com/pluralsight/Meat.java
+++ src/main/java/com/pluralsight/Meat.java
@@ -1,22 +1,53 @@
 package com.pluralsight;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Meat extends Ingredient {
 
+    private double smallBase;
+    private double smallExtra;
+    private double mediumBase;
+    private double mediumExtra;
+    private double largeBase;
+    private double largeExtra;
 
     public Meat(String name, boolean isExtra, boolean isPremium, String type) {
         super(name, isExtra, true, type);
+
+        this.smallBase = 1.00;
+        this.smallExtra = 0.50;
+        this.mediumBase = 2.00;
+        this.mediumExtra = 1.00;
+        this.largeBase = 3.00;
+        this.largeExtra = 1.50;
     }
 
 
 
-    public double getPrice(MeatSize size){
+    public double getPrice(Size size){
 
-        double meatPrice = size.getBasePrice();
+       double base = 0;
+       double extraCharge = 0;
+
+        if(size == Size.SMALL){
+            base = this.smallBase;
+            extraCharge = this.smallExtra;
+        } else if (size == Size.MEDIUM) {
+            base = this.mediumBase;
+            extraCharge = this.mediumExtra;
+
+        } else if (size == Size.LARGE) {
+            base = this.largeBase;
+            extraCharge = this.largeExtra;
+
+        }
+
         if(isExtra()){
-            meatPrice += size.getExtra();
+            base += extraCharge;
         }
 
 
-        return meatPrice;
+        return base;
     }
 }
