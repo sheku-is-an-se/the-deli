446cc052e0c35abffb366873ba1d01d5300e92df
sheku-is-a-se <sheku2026@gmail.com>
2026-05-27 20:18:38 -0400

    Complete getPrice method for Meat class

--- src/main/java/com/pluralsight/Meat.java
+++ src/main/java/com/pluralsight/Meat.java
@@ -4,6 +4,19 @@ public class Meat extends Ingredient {
 
 
     public Meat(String name, boolean isExtra, boolean isPremium, String type) {
-        super(name, isExtra, isPremium, type);
+        super(name, isExtra, true, type);
+    }
+
+
+
+    public double getPrice(MeatSize size){
+
+        double meatPrice = size.getBasePrice();
+        if(isExtra()){
+            meatPrice += size.getExtra();
+        }
+
+
+        return meatPrice;
     }
 }
